c6aac35010764684fed81b8ebd1911594db90114
Marci Clark <marcig612@gmail.com>
2019-01-31 04:00:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  
+  def name=(dogs_name)
+    @name = snoopy
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = pug
+  end
+  
+  def breed
+    @breed
+  end 
+end
